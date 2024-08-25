4a830ee2e974c169ddea97db09bb3fc2be0ed9dd
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-08-25 21:34:22 +0700

    Add UserButton sign-out

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
