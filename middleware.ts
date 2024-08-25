8e9518e071519cdc76527d01f7c814d61cdb47f8
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-08-25 20:58:27 +0700

    Make the sign-up and sign-in routes public

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// Define routes that should be public
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect(); // Protect all routes except the public ones
+  }
+});
+
+// Configuration for the matcher
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
